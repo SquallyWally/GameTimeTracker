1ec292832efb53f91136bb05692e5665c5ad88f0
milo <milo.kastablank@protonmail.com>
2022-09-21 21:17:08 +0200

    Created CRUD Api and start for WebScraper in Typescript

---
+++ HowLongToBeat.Api/Program.cs
@@ -0,0 +1,33 @@
+using HowLongToBeat.Api.Context;
+using HowLongToBeat.Api.Repositories;
+using HowLongToBeat.Api.TrackerService;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<GameContext>(opt => opt.UseSqlServer("Server=.;Database=GameTracker;MultipleActiveResultSets=True;Trusted_Connection=True;"));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+builder.Services.AddScoped<GameTrackerService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
