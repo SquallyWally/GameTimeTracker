30f4988bf9145051067007b5a071c63fc409a116
milo <milo.kastablank@protonmail.com>
2022-09-21 21:24:17 +0200

    Added node modules and Repositories

---
+++ HowLongToBeat.Api/TrackerService/GameTrackerService.cs
@@ -0,0 +1,41 @@
+﻿using HowLongToBeat.Api.Models;
+using HowLongToBeat.Api.Repositories;
+
+namespace HowLongToBeat.Api.TrackerService
+{
+    public class GameTrackerService
+    {
+        private readonly IGameRepository _gameRepository;
+
+        public GameTrackerService(IGameRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+
+        //GetGame
+        public Task<Game?> GetGame(int id)
+        {
+            return _gameRepository.FindById(id);
+        }
+        //GetGames
+        public async Task<IEnumerable<Game>> GetGames()
+        {
+            return await _gameRepository.FindAll();
+        }
+        //CreateGame
+        public  Task<Game> CreateGame(Game game)
+        {
+            return  _gameRepository.InsertGame(game);
+        }
+        //EditGameTime
+        public async Task<Game?> EditGameTime(int id, Game game)
+        {
+            return await _gameRepository.EditGameTime(id, game);
+        }
+        //DeleteGame
+        public async Task<Game?> DeleteGame(int id)
+        {
+            return await _gameRepository.DeleteGame(id);
+        }
+    }
+}
---
+++ WebScraperAPI/howlongtobeat.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const howlongtobeat_1 = require("howlongtobeat");
+let hltbService = new howlongtobeat_1.HowLongToBeatService();
+//https://github.com/ckatzorke/howlongtobeat
+// Stappenplan wat ik hiermee doet
+//# sourceMappingURL=howlongtobeat.js.map
